bf173becc786fddb2148a7fdcdb8e43a41a4ea4a
あさあとうま <asaatouma@asaatoumanoMacBook-Pro.local>
2021-07-04 11:04:47 +0900

    javascript課題1-3一応終了しました

---
+++ aa.js
@@ -0,0 +1,30 @@
+const houseplas = [, , , , , , , , , , , , , , , , , , , , , , , , , ,];
+const result = houseplas.join('=');
+console.log(result);
+const tsk = ['現在持っているタスク一覧'];
+console.log(tsk[0])
+const jpintest = [, , , , , , , , , , , , , , , , , , , , , , , , , ,];
+const equal = jpintest.join('=');
+console.log(equal);
+
+
+const housework = ['掃除', '買い物', '散歩', []];
+console.log('0 : ' + housework[0]);
+console.log('1 : ' + housework[1]);
+console.log('2 : ' + housework[2]);
+
+const house = prompt("「確認　追加　削除　終了」の4ついづれかを追加してください");
+window.alert('新しいタスクが追加されました"');
+
+const semicoron = [, , , , , , , , , , , , , , , , , , , , , , , , , ,];
+const houseequal = semicoron.join('=');
+console.log(houseequal);
+const hostsk = ['現在持っているタスク一覧'];
+console.log(hostsk[0])
+const conversion = [, , , , , , , , , , , , , , , , , , , , , , , , , ,];
+const hassemicoron = conversion.join('=');
+console.log(hassemicoron);
+console.log('0 : ' + housework[0]);
+console.log('1 : ' + housework[1]);
+console.log('2 : ' + housework[2]);
+console.log(('3 : ' + housework[3]) + house);
